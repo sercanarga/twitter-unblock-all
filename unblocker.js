b9544307ec00d7ebb2aacd139d9b38e7fcea0efe
Sercan ARĞA <sercanarga@icloud.com>
2019-05-28 19:22:58 +0300

    Update unblocker.js

--- unblocker.js
+++ unblocker.js
@@ -5,28 +5,33 @@ async function click(btn) {
 }
 
 async function unblock(timeOut = 500) {
-    let pScroll, scroll = window.scrollY;
-    do {
-        window.scrollTo(0, document.body.scrollHeight);
-        pScroll = scroll;
-        await new Promise(resolve => setTimeout(resolve, timeOut));
-        scroll = window.scrollY;
-    } while((scroll - pScroll) > 0);
+    if (window.location.href === 'https://twitter.com/settings/blocked') {
+        let pScroll, scroll = window.scrollY;
+        do {
+            window.scrollTo(0, document.body.scrollHeight);
+            pScroll = scroll;
+            await new Promise(resolve => setTimeout(resolve, timeOut));
+            scroll = window.scrollY;
+        } while((scroll - pScroll) > 0);
 
-    let buttons = document.querySelectorAll('.blocked-text');
-    if (buttons.length > 0) {
-        let check1 = confirm('Do you want to unblock all ' + buttons.length + ' accounts?');
-        if (check1) {
-            let check2 = prompt('Are you sure? [y/N]');
-            if (check2 === 'y' || check2 === 'Y') {
-                click(buttons);
+        let buttons = document.querySelectorAll('.blocked-text');
+        if (buttons.length > 0) {
+            let check1 = confirm('Do you want to unblock all ' + buttons.length + ' accounts?');
+            if (check1) {
+                let check2 = prompt('Are you sure? [y/N]');
+                if (check2 === 'y' || check2 === 'Y') {
+                    click(buttons);
+                } else {
+                    alert('Ok!');
+                }
             } else {
                 alert('Ok!');
             }
         } else {
-            alert('Ok!');
+            alert('No blocked account!');
         }
     } else {
-        alert('No blocked account!');
+        alert('You are redirecting to settings/blocked. Run the script again.');
+        document.location.href = 'https://twitter.com/settings/blocked';
     }
 }
